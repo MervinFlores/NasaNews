62c2e06fffbe70acc06919bb6b4447c2f369ac7e
Mervin Flores <mervinfloresap@gmail.com>
2024-02-29 23:35:14 -0400

    Update View+Utils.swift, deleted comments and whitespaces

--- NasaNews/Helpers/View+Utils.swift
+++ NasaNews/Helpers/View+Utils.swift
@@ -9,11 +9,6 @@ import UIKit
 
 extension UIView {
     
-//    override func layoutSubviews() {
-//        super.layoutSubviews()
-//        self.gradientLayer?.frame = self.bounds
-//    }
-    
     func applyBorderWithCornerRadius(borderWidth: CGFloat = 1.0, borderColor: UIColor = .lightGray, cornerRadius: CGFloat = 8.0) {
         self.layer.borderWidth = borderWidth
         self.layer.borderColor = borderColor.cgColor
@@ -21,12 +16,10 @@ extension UIView {
     }
     
     func applyGradient(from startColor: UIColor, to endColor: UIColor, startEdge: UIRectEdge, endEdge: UIRectEdge) {
-            // Crear gradient layer
             let gradientLayer = CAGradientLayer()
             gradientLayer.colors = [startColor.cgColor, endColor.cgColor]
             gradientLayer.frame = self.bounds
 
-            // Ajustar puntos de inicio y fin según los bordes especificados
             switch (startEdge, endEdge) {
                 case (.top, .bottom):
                     gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
@@ -45,22 +38,16 @@ extension UIView {
                     gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
             }
 
-            // Asegurar que el gradiente cubra el view completamente incluso después de cambios de layout
             gradientLayer.frame = self.bounds
             self.layer.insertSublayer(gradientLayer, at: 0)
             
-            // Ajustar el gradiente a las esquinas redondeadas
             self.layer.cornerRadius = self.layer.cornerRadius
             self.layer.masksToBounds = true
             
-            // Actualizar el frame del gradientLayer en cambios de layout
             gradientLayer.frame = self.bounds
             
-            // Remover cualquier gradient layer anterior para evitar duplicados
             self.layer.sublayers?.removeAll(where: { $0 is CAGradientLayer })
             self.layer.insertSublayer(gradientLayer, at: 0)
         }
-
-        
 }
 
